acc3a300ab57a6aa195a7b98463c800d2357eae0
John Boehr <jbboehr@gmail.com>
2015-04-04 11:16:41 -0700

    Initial commit

---
+++ src/net/johnboehr/netbeans/handlebars/filetype/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Other", content = "HandlebarsTemplate.hbs")
+package net.johnboehr.netbeans.handlebars.filetype;
+
+import org.netbeans.api.templates.TemplateRegistration;
